d86a31dc4255a94a8f1decb1205ee19fec63f7fc
zia-kayani <ziakayani.official@gmail.com>
2025-12-22 03:46:09 -0800

    Generate the code with codegen and run the test

---
+++ tests/test_recorded.py
@@ -0,0 +1,12 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("Admin")
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("admin123")
+    page.get_by_role("button", name="Login").click()
+    page.get_by_role("link", name="Admin").click()
