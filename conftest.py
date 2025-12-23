b2a75e13448c8302427d714832d481872520738c
zia-kayani <ziakayani.official@gmail.com>
2025-12-23 00:11:58 -0800

    Modify conftest file so that tracing=on should work

--- conftest.py
+++ conftest.py
@@ -1,16 +1,33 @@
+
 import pytest
 from playwright.sync_api import sync_playwright
 
-
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser =  p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
 
+# #Added this fixture so that my tracing should work 
+# #this will create the seperate context of the browser 
 @pytest.fixture
-def page(browser):
-    page = browser.new_page()
+def context(browser):
+    context = browser.new_context()
+
+    context.tracing.start(
+        screenshots=True,
+        snapshots=True,
+        sources=True
+    )
+
+    yield context
+
+    context.tracing.stop(path="trace.zip")
+    context.close()
+
+@pytest.fixture
+def page(context):
+    page = context.new_page()
     yield page
     page.close()
