9adcd61270d8eea290254458b1a27cc67366d28a
zia-kayani <ziakayani.official@gmail.com>
2025-12-23 07:38:05 -0800

    data driven testing with json file

--- tests/test_data_driven_testing.py
+++ tests/test_data_driven_testing.py
@@ -12,12 +12,20 @@ def get_csv_data() -> list:
             data.append(row)
         return data
 
+#Json file data
+def get_json_data():
+    import json
+    with open('./test-data/data.json', 'r') as fl:
+        data =  json.load(fl)
+        return ((item['username'], item['password']) for item in data)
+
 @pytest.mark.parametrize('username,password', 
 # [
 #     ('Admin', 'admin123'),
 #     ('naila', 'naila123')
 # ]
-get_csv_data()
+# get_csv_data()
+get_json_data()
 )
 
 def test_example(page: Page, username, password) -> None:
