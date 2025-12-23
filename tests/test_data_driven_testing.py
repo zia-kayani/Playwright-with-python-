5ac9bcb1b5cec5c9cfc0e745e3ee2523f00dfb7b
zia-kayani <ziakayani.official@gmail.com>
2025-12-23 07:09:10 -0800

    data driven testing with parametrize marker

---
+++ tests/test_data_driven_testing.py
@@ -0,0 +1,17 @@
+import re
+import pytest
+from playwright.sync_api import Page, expect
+
+
+@pytest.mark.parametrize('username,password', [
+    ('Admin', 'admin123'),
+    ('naila', 'naila123')
+])
+def test_example(page: Page, username, password) -> None:
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill(username)
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill(password)
+    page.get_by_role("button", name="Login").click()
+    expect(page.get_by_role("link", name="Dashboard")).to_be_visible()
