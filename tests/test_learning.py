d894f45492104dc29cf0631a9164c6491b18637c
zia-kayani <ziakayani.official@gmail.com>
2026-01-06 03:57:41 -0800

    networking waiting and default and explicit timeout for elements

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -10,3 +10,20 @@ def test_timeout(page):
     end_time = time.time()
     print(f'start time is {start_time} and end time is {end_time} and total time taken is = {end_time - start_time}')
 
+    #------------ implicit waiting for an elements based on selector -------
+    page.wait_for_selector('div#message')   #waits until the element is present in the DOM
+    page.wait_for_selector('div#message', state='visible')  # waits until the element is visible on the page
+    page.wait_for_selector('div#message', state='hidden')  # waits until the element is hidden on the page
+
+    #------------------- Network waitings -----------------
+    with page.expect_response('**/api/data') as response_info:
+        page.click('text=Fetch Data')
+
+    response = response_info.value
+    print(response.json())
+
+
+  #------- defautl and explicit timeout -----
+    page.set_default_timeout(5000) # set default timeout for all the operations
+    page.wait_for_selector('div#message', timeout=2000) # explicit timeout for a particular operation
+    
